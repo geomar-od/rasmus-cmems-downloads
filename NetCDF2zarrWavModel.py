e69a6244be6c8453355a3253bdc5651cee44784d
El Sh <eshchekinova@geomar.de>
2021-02-08 12:39:22 +0100

    Time extraction is simplified

--- NetCDF2zarrWavModel.py
+++ NetCDF2zarrWavModel.py
@@ -11,8 +11,7 @@ from dask.distributed import Client, progress, LocalCluster
 import pandas as pd
 from pathlib import Path
 import dask.array
-import xarray
-
+import pyarrow.parquet as pq
 import xarray as xr
 import numpy as np
 import numcodecs
@@ -20,34 +19,6 @@ import zarr
 from datetime import datetime
 import time
 import sys
-
-def change_time_format(time_f):
-    # change format from "yyyy-MM-dd HH:mm:SS" to
-    # ISO 8601 notation: {yyyy}-{MM}-{dd}T{HH}:{mm}:{SS}Z or
-    # ISO 8601 date: {yyyy}-{MM}-{dd}
-    time_ISO8601=f"{time_f[0:10]}T{time_f[11:19]}Z"
-    time_ISO8601_short=f"{time_f[0:10]}"
-    return time_ISO8601_short
-
-def mkdate(datestr):
-   # function make time string for argument parser
-   try:
-    return datestr
-   except ValueError:
-    raise argparse.ArgumentTypeError(f"{datestr} time start is not in a proper format dd/MM/yy HH:mm:SS")
-
-def convert_to_secs(tt):
-   # The input time should be given in format "{yyyy}-{MM}-{dd}"
-   time_format = f"{tt[8:10]}/{tt[5:7]}/{tt[0:4]} 00:00:00"
-   obj2 = time.strptime(time_format,"%d/%m/%Y %H:%M:%S")
-   time_secs = time.mktime(obj2)
-   return time_secs
-
-def extract_time(input_files, product_id):
-   sorted_files = sorted(input_files)
-   start_day = str(sorted_files[0]).replace('.nc','')[-10:]
-   end_day = str(sorted_files[-1]).replace('.nc','')[-10:]
-   return start_day, end_day
 
 if __name__ == "__main__":
 
@@ -98,7 +69,12 @@ if __name__ == "__main__":
     # set list of variables
     variables=["VPED", "VSDX", "VSDY"]
 
-    start_day, end_day = extract_time(sorted(path_in_dir.glob("*.nc")), product_id)
+    # convert files in nc directory to dataset using xarray
+    nc_ds = xr.open_mfdataset(input_files, parallel=True, concat_dim="time", data_vars="minimal",combine='nested')
+
+    start_day = pd.to_datetime(nc_ds.time.min().values)
+    end_day = pd.to_datetime(nc_ds.time.max().values)
+    end_day =datetime(end_day.year,end_day.month,end_day.day+1)
 
     # set zarr store name
     for variable_name in variables:
